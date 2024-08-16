b10bbdad2797dc5812711ce956bbb91d1d99f130
Sydney Sanders <ssanderss444@gmail.com>
2024-08-16 18:56:17 -0400

    update the testimonal

--- my-app/src/app/components/TestimonialContainer.tsx
+++ my-app/src/app/components/TestimonialContainer.tsx
@@ -47,7 +47,7 @@ export default function TestimonialContainer() {
     },
     {
       quote:
-        "Rio provided unparalleled clarity on Crypt, Dropping gold gems, knows what he is talking about",
+        "Rio provided unparalleled clarity on Crypto, Dropping gold gems, knows what he is talking about",
         author: "Mike",
 
 

b9b91979daab44829e5d6e856b0ad04487abb33a
Sydney Sanders <ssanderss444@gmail.com>
2024-08-20 14:44:25 -0400

    update the layout

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -9,7 +9,7 @@ import { Gnosis, Ethereum } from "@thirdweb-dev/chains";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Black W3B | Blockchain Consultations",
+  title: "Web3 Learning| BlackW3B",
   description:
     "Empowering Black communities through Web3 consulting. Gain knowledge in blockchain, DeFi, and cryptocurrency to take control of your financial future. Learn, build, and grow with expert guidance tailored to those in need.",
     icons: {
